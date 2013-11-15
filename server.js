4f5c9bac6e60518ebda2800af1f723cd045b74cb
Manuel Palacin <manuelpalacin@gmail.com>
2013-11-15 10:18:40 +0100

    Update deprecated express.createServer() and WS
    We have updated deprectated express.createServer() in server.js. We have
    also created a REST-JSON webservice with full and lite results.

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ var express = require('express'),
   hostnamefinder = require("./lib/hostnamefinder.js").hostnamefinder;
 
 
+var app = express(); 
 
+//app.use(express.bodyParser()); 
+//bodyParser will be removed by the following 2 lines
+app.use(express.json());
+app.use(express.urlencoded());
 
-var app = express.createServer();
-app.use(express.bodyParser());
 app.post('/', function(req, res){
   console.log(new Date(), req.connection.remoteAddress)
   console.log(new Date(), req.body.url)
@@ -46,7 +49,47 @@ app.get('/', function(req, res){
 });
 
 
-app.listen(1337);
+var url = require("url");
+
+app.get('/export', function(req, res){
+	console.log(new Date(), req.connection.remoteAddress)
+  
+	var pathname = url.parse(req.url, true).query["domain"];
+
+	completecdnfinder(pathname, function(err, results){
+		if(results){
+		  res.send(results);
+		} else {
+		  res.send({"status": "FAILURE"});
+		}
+	});
+
+});
+
+app.get('/links', function(req, res){
+	console.log(new Date(), req.connection.remoteAddress)
+  
+	var pathname = url.parse(req.url, true).query["domain"];
+
+	completecdnfinder(pathname, function(err, results){
+		if(results){
+			for(var i = 0, l = results.everything.length; i < l; i++) {
+				delete results.everything[i].headers;
+			}
+			
+			res.send(results);
+		} else {
+			res.send({"status": "FAILURE"});
+		}
+	});
+
+});
+
+
+
+var port = 1337;
+console.log('CDNFinder listening in http://yourdomain:'+port);
+app.listen(port);
 
 //completecdnfinder("http://www.msnbc.com/", function(err, results){
 //  console.dir(results);
